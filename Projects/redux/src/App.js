c89fa0bfdef67bd059ffd788f32dfdd194f21ed0
M0mahmoud <houdmohamed85@gmail.com>
2022-10-27 15:30:36 +0200

    End Redux Basics

--- Projects/redux/src/App.js
+++ Projects/redux/src/App.js
@@ -1,8 +1,23 @@
+import UserProfile from "./Components/UserProfile";
 import Counter from "./Components/Counter";
+import Header from "./Components/Header";
+import Auth from "./Components/Auth";
+
+import { useSelector } from "react-redux";
+
 import "./App.css";
 
 function App() {
-  return <Counter />;
+  const auth = useSelector((state) => state.auth.auth);
+
+  return (
+    <>
+      <Header />
+      {!auth && <Auth />}
+      {auth && <UserProfile />}
+      {auth && <Counter />}
+    </>
+  );
 }
 
 export default App;
