7c2e1914912eb53e2ddfd28b28ec0fd65b0749eb
M0mahmoud <houdmohamed85@gmail.com>
2022-11-05 15:38:01 +0200

    Finish App

--- Projects/advanced_router/src/components/comments/Comments.js
+++ Projects/advanced_router/src/components/comments/Comments.js
@@ -1,25 +1,62 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-import classes from './Comments.module.css';
-import NewCommentForm from './NewCommentForm';
+import NewCommentForm from "./NewCommentForm";
+import CommentsList from "./CommentsList";
+import LoadingSpinner from "../UI/LoadingSpinner";
+
+import useHttp from "../../hooks/use-http";
+import { getAllComments } from "../../lib/api";
+
+import classes from "./Comments.module.css";
 
 const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false);
+  const params = useParams();
+  const { quoteId } = params;
+  const { sendRequest, status, data: loadedComments } = useHttp(getAllComments);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [quoteId, sendRequest]);
 
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
-  
+
+  const addedComments = useCallback(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  let comments;
+
+  if (status === "pending") {
+    comments = (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (status === "completed" && (loadedComments || loadedComments > 0)) {
+    comments = <CommentsList comments={loadedComments} />;
+  }
+
+  if (status === "completed" && (!loadedComments || loadedComments === 0)) {
+    comments = <p className="centered">No Comments</p>;
+  }
   return (
     <section className={classes.comments}>
       <h2>User Comments</h2>
       {!isAddingComment && (
-        <button className='btn' onClick={startAddCommentHandler}>
+        <button className="btn" onClick={startAddCommentHandler}>
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm />}
-      <p>Comments...</p>
+      {isAddingComment && (
+        <NewCommentForm onAddedComment={addedComments} quoteId={quoteId} />
+      )}
+      {comments}
     </section>
   );
 };
--- Projects/advanced_router/src/pages/QuotesDetail.js
+++ Projects/advanced_router/src/pages/QuotesDetail.js
@@ -1,29 +1,48 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link, Route, useParams, useRouteMatch } from "react-router-dom";
+
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
-
 import Comments from "../components/comments/Comments";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 
-const DUMMY_QUOTES = [
-  { id: "q1", author: "Max", text: "Learning React is fun!" },
-  { id: "q2", author: "Maximilian", text: "Learning React is great!" },
-];
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
 
 const QuotesDetail = () => {
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
+
   const params = useParams();
   const { quoteId } = params;
   const match = useRouteMatch(); // return Params & Path & URL
-  console.log("match", match);
 
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === quoteId);
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
 
-  if (!quote) {
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered">{error}</p>;
+  }
+
+  if (!loadedQuote.text) {
     return <p>No Quote Found...!!</p>;
   }
 
   return (
     <>
-      <HighlightedQuote text={quote.text} author={quote.author} />
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
       <Route path={`${match.path}`} exact>
         <div className="centered">
           <Link className="btn--flat" to={`${match.url}/comments`}>
